948b0234d88bb3bb60fe90d4798c39e5731c89bd
Vsevolod-Rusinskiy <leto99999@gmail.com>
2025-04-18 16:33:12 +0300

    refactor: enhance StatisticsBlokUnit component with loading state and spinner for improved user experience

--- frontend/src/2_widgets/statistics/ui/statistics-block-unit.tsx
+++ frontend/src/2_widgets/statistics/ui/statistics-block-unit.tsx
@@ -1,8 +1,12 @@
+import { Spinner } from '@/5_shared/ui/spinner'
+
 export function StatisticsBlokUnit({title, value, loading}: {title: string, value: string, loading: boolean} ) {
   return (
-    <div className="border rounded p-3 bg-white">
-      <div className="text-sm text-gray-500">{title}</div>
-      <div className="font-mono text-lg text-gray-500">{loading ? '...' : value}</div>
+    <div className="border rounded p-3 bg-white flex flex-col items-center justify-center min-h-[70px]">
+      <div className="text-sm text-gray-500 mb-1">{title}</div>
+      <div className="font-mono text-lg text-gray-500 min-h-[28px] flex items-center justify-center">
+        {loading ? <Spinner /> : value}
+      </div>
     </div>
   )
 } 
